f54897c2e2230b100d6048b64962fce9e019fa8b
hong0980 <hong0980@qq.com>
2020-04-29 16:08:50 +0800

    up

--- applications/luci-app-aria2/luasrc/model/cbi/aria2/files.lua
+++ applications/luci-app-aria2/luasrc/model/cbi/aria2/files.lua
@@ -10,11 +10,22 @@ local uci  = require "luci.model.uci".cursor()
 local config_dir = uci:get("aria2", "main", "config_dir") or "/var/etc/aria2"
 local config_file = "%s/aria2.conf.main" % config_dir
 local session_file = "%s/aria2.session.main" % config_dir
+local a = "/etc/config/aria2" % config_dir
 
 m = SimpleForm("aria2", "%s - %s" % { translate("Aria2"), translate("Files") },
 	translate("Here shows the files used by aria2."))
 m.reset = false
 m.submit = false
+
+s = m:section(SimpleSection, nil, translatef("config文件的内容: <code>%s</code>", a))
+
+o = s:option(TextValue, "_session")
+o.rows = 20
+o.readonly = true
+o.cfgvalue = function()
+	local v = fs.readfile(a) or translate("File does not exist.")
+	return util.trim(v) ~= "" and v or translate("Empty file.")
+end
 
 s = m:section(SimpleSection, nil, translatef("Content of config file: <code>%s</code>", config_file))
 
