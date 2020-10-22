f0a4067851d1d797ca136e195a18f87cc3a6ea82
hong0980 <hong0980@qq.com>
2020-10-22 12:47:37 +0800

    qnap_boot

--- applications/luci-app-aria2/luasrc/controller/aria2.lua
+++ applications/luci-app-aria2/luasrc/controller/aria2.lua
@@ -1,42 +1,34 @@
-local n=require"nixio.fs"
-local a=require"luci.sys"
-local e=require"luci.http"
-local i=require"luci.util"
-local o=require"luci.model.uci".cursor()
+local ucic = luci.model.uci.cursor()
+local http = require "luci.http"
+local util = require "luci.util"
 module("luci.controller.aria2",package.seeall)
+
 function index()
-if not nixio.fs.access("/etc/config/aria2")then
-return
+	if not nixio.fs.access("/etc/config/aria2")then return end
+	entry({"admin","nas","aria2"},firstchild(),_("Aria2")).dependent=false
+	entry({"admin","nas","aria2","config"},cbi("aria2/config"),_("Configuration"),1)
+	entry({"admin","nas","aria2","file"},form("aria2/files"),_("Files"),2)
+	entry({"admin","nas","aria2","log"},firstchild(),_("Log"),3)
+	entry({"admin","nas","aria2","log","view"},template("aria2/log_template"))
+	entry({"admin","nas","aria2","log","read"},call("action_log_read"))
+	entry({"admin","nas","aria2","status"},call("action_status"))
 end
-entry({"admin","nas","aria2"},
-firstchild(),_("Aria2")).dependent=false
-entry({"admin","nas","aria2","config"},
-cbi("aria2/config"),_("Configuration"),1)
-entry({"admin","nas","aria2","file"},
-form("aria2/files"),_("Files"),2)
-entry({"admin","nas","aria2","log"},
-firstchild(),_("Log"),3)
-entry({"admin","nas","aria2","log","view"},
-template("aria2/log_template"))
-entry({"admin","nas","aria2","log","read"},
-call("action_log_read"))
-entry({"admin","nas","aria2","status"},
-call("action_status"))
-end
+
 function action_status()
 local t={
-running=(a.call("pidof aria2c >/dev/null")==0)
+running=(luci.sys.call("pidof aria2c >/dev/null")==0)
 }
-e.prepare_content("application/json")
-e.write_json(t)
+http.prepare_content("application/json")
+http.write_json(t)
 end
+
 function action_log_read()
-local t={log="",syslog=""}
-local o=o:get("aria2","main","log")or"/var/log/aria2.log"
-if n.access(o)then
-t.log=i.trim(a.exec("tail -n 50 %s | sed 'x;1!H;$!d;x'"%o))
-end
-t.syslog=i.trim(a.exec("logread | grep aria2 | tail -n 50 | cut -d' ' -f3- | sed -e 's/daemon.info.aria2: /aria2日志信息：/g' -e 's/daemon.err aria2c.*]:/aria2错误信息：/g' -e '/S99aria2/d'"))
-e.prepare_content("application/json")
-e.write_json(t)
+	local t={log="",syslog=""}
+	local o=ucic:get("aria2","main","log")or"/var/log/aria2.log"
+		if nixio.fs.access(o) then
+		t.log=util.trim(luci.sys.exec("tail -n 50 %s | sed 'x;1!H;$!d;x'"%o))
+		end
+	t.syslog=util.trim(luci.sys.exec("[ -f '/var/log/aria2_1.log' ] && cat /var/log/aria2_1.log"))
+	http.prepare_content("application/json")
+	http.write_json(t)
 end
